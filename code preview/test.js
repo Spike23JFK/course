ce9d9a1ca4637d6057ed2d8baee2c6ed27fffb38
Josef Kameník <34886937+Spike23JFK@users.noreply.github.com>
2018-03-06 14:15:05 +0100

    Add files via upload

---
+++ code preview/test.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What's your first name?");
+var lastName = prompt("what's your last name?");
+var age = prompt("what's your age?");
+
+alert("hello " + age + " year old " + firstName + " " + lastName);
